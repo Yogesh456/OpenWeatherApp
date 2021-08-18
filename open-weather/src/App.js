81d6f5757544245e5ad1e6504a934e5715c6b118
unknown <ytech250@gmail.com>
2021-08-18 11:12:07 +0530

    Weather App Redy

--- open-weather/src/App.js
+++ open-weather/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 
-  
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 const api = {
   key: "c2c76e50ba38e96fa62e9a63f78a8a02",
   base: "https://api.openweathermap.org/data/2.5/"
@@ -12,8 +12,18 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
   const [News, setNews] = useState('Nonews')
-  const [News2,setNews2] = useState('NoNews') 
- 
+  const [counter, setCounter] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      
+   setCounter(counter + 1); }, 5000);
+   return () => {
+     clearInterval(interval);
+   };
+   
+  });
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
@@ -23,19 +33,20 @@ function App() {
           setQuery('');
           console.log(result);
         });
-         fetch ('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json').then(news => news.json()).then(todayNews => {setNews(todayNews);
-            console.log(todayNews)} 
-              );
-            
-            
-        
-          
-         
+      fetch('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json').then(news => news.json()).then(todayNews => {
+        setNews(todayNews);
+        console.log(todayNews)
+      }
+      );
+
+
+
+
 
 
     }
   }
-            
+
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -53,7 +64,7 @@ function App() {
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Search..."
@@ -63,33 +74,37 @@ function App() {
           />
         </div>
         {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-        </div>
         ) : ('')}
-        
+
         {(typeof News.articles != "undefined") ? (
-          <div className="NewsBox" >
-       <div className='img-box'> <img src={News.articles[2].urlToImage}/> </div>
-          <h3>  {News.articles[2].title}</h3> 
-          <h4> Source :  {News.articles[2].author}</h4>
-            
-        
-            </div>
+         <div className = "gradiant">
+<div className="newsbox" >
+  <div className='image'  ><div>
+    <h3 className='author' >{News.articles[counter].author}</h3> 
+     <p className='title' >{News.articles[counter].title}</p>
+     <a className='link' href={News.articles[counter].url}>Read More ...</a>
+    
+    </div> </div>
+</div>
+
+         </div>
 
         ) : ('')}
-        
+
       </main>
-      
+
 
     </div>
   );
